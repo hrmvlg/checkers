965b4cd7239d9ef3c76f39a6c42f7548dc800296
Olya Khromova <olya.hrmvlg@gmail.com>
2024-10-30 17:28:27 +0300

    added regular move function

--- main.js
+++ main.js
@@ -34,11 +34,7 @@ function initializeBoard() {
   }
 }
 
-function onCellClick() {
-
-}
-
-function addPiece(cell, color){
+function addPiece(cell, color) {
   const piece = document.createElement("div");
   piece.classList.add('piece', color);
 
@@ -46,6 +42,112 @@ function addPiece(cell, color){
   piece.dataset.king = false;
 
   cell.appendChild(piece);
+}
+
+function onCellClick(e) {
+  const cell = e.target.closest(".cell");
+  if (!cell) return;
+  const piece = cell.querySelector(".piece");
+
+  if (selectedPiece) {
+    if (canMove(selectedPiece, cell)) {
+      movePiece(selectedPiece, cell);
+      selectedPiece = null;
+    }
+    else {
+      selectedPiece = null;
+    }
+  }
+  else {
+    if (piece && piece.dataset.color === turn) {
+      selectedPiece = piece;
+    }
+  }
+}
+
+function canMove(piece, targetCell) {
+  const startRow = parseInt(piece.parentElement.dataset.row);
+  const startCol = parseInt(piece.parentElement.dataset.col);
+
+  const endRow = parseInt(targetCell.dataset.row);
+  const endCol = parseInt(targetCell.dataset.col);
+
+  const direction = piece.dataset.color === "white" ? -1 : 1;
+
+  const isKing = piece.dataset.king === "true";
+
+  if (targetCell.querySelector(".piece")) {
+    console.log("клетка занята");
+    return false;
+  }
+
+  if (!isKing) {
+    return canMoveRegular(piece, startRow, startCol, endRow, endCol, direction);
+  }
+  else {
+    return canMoveKing(piece, startRow, startCol, endRow, endCol);
+  }
+}
+
+function canMoveRegular(piece, startRow, startCol, endRow, endCol, direction) {
+  if (Math.abs(endRow - startRow) === 1 &&
+    Math.abs(endCol - startCol) === 1 &&
+    endRow - startRow === direction
+  ) {
+    return true;
+  }
+
+  if (Math.abs(endRow - startRow) === 2 &&
+    Math.abs(endCol - startCol) === 2 &&
+    endRow - startRow === 2 * direction
+  ) {
+    const middleRow = (startRow + endRow) / 2;
+    const middleCol = (startCol + endCol) / 2;
+
+    const middleCell = document.querySelector(`.cell[data-row="${middleRow}"][data-col="${middleCol}"]`);
+    const middlePiece = middleCell.querySelector(".piece");
+
+    if (middlePiece && middleCell.dataset.color !== piece.dataset.color) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function movePiece(piece, targetCell) {
+  const startRow = parseInt(piece.parentElement.dataset.row);
+  const startCol = parseInt(piece.parentElement.dataset.col);
+
+  const endRow = parseInt(targetCell.dataset.row);
+  const endCol = parseInt(targetCell.dataset.col);
+
+  if (Math.abs(endRow - startRow) === 2 && Math.abs(endCol - startCol) === 2) {
+    const middleRow = (startRow + endRow) / 2;
+    const middleCol = (startCol + endCol) / 2;
+
+    const middleCell = document.querySelector(`.cell[data-row="${middleRow}"][data-col="${middleCol}"]`);
+    const middlePiece = middleCell.querySelector(".piece");
+
+    if (middlePiece) {
+      middleCell.removeChild(middlePiece);
+    }
+  }
+
+  targetCell.appendChild(piece);
+
+  if((piece.dataset.color === "white" && endRow === 0) || (piece.dataset.color === "black" && endRow === 7)){
+    piece.dataset.king = "true";
+    piece.classList.add("king");
+  }
+
+  turn = turn === "white" ? "black" : "white";
+  selectedPiece = null;
+
+  checkGameState();
+}
+
+function checkGameState(){
+  return true;
 }
 
 initializeBoard();
